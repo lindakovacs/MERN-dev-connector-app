e84229a31aea49b3cb7be3f22929df009b22f29b
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-18 00:15:14 -0500

    Add Express Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//@route    GET api/use
+//@route    POST api/use
 //@desc     Test route
 //@access   Public
-router.post('/', (req, res) => {
+router.post(
+  '/',
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
+
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     console.log(req.body);
     res.send('User route');
-});
+  }
+);
 
 module.exports = router;
