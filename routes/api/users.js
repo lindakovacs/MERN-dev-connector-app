f912606be72ccf3b2dd91e0004001b0c3cd21280
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-18 00:30:02 -0500

    Implement User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
 
-//@route    POST api/use
-//@desc     Test route
-//@access   Public
+const User = require('../../models/User');
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
   '/',
   check('name', 'Name is required').notEmpty(),
@@ -13,14 +20,55 @@ router.post(
     'password',
     'Please enter a password with 6 or more characters'
   ).isLength({ min: 6 }),
-
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('User route');
+
+    //Retrieve the info from post request
+    const { name, email, password } = req.body;
+
+    try {
+      //Check in the DB whether user already exists or not
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get the avatar info
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm',
+        }),
+        { forceHttps: true }
+      );
+
+      // Prepare user template to be stored in DB
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save the user registration details to DB
+      await user.save();
+
+      res.send('User Registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
