5709b025a747043f3cc37249c965b01f4b1fd71d
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 14:26:27 -0500

    Add New Post

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router = express.Router();
-const auth = require('../../middleware/auth');
-const User = require('../../models/User');
-
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const router = express.Router();
+const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route    GET api/auth
 // @desc     Get user by token
