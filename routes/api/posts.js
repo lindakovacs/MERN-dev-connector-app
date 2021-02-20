7f5485909b84069eed9f458f08eb986a6d8a20fc
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 14:47:43 -0500

    Delete a Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -76,4 +76,33 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route    DELETE api/posts/:id
+// @desc     Delete a Post
+// @access   Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Check user owner to delete the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+    // Check if a post exist with the required id.
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
