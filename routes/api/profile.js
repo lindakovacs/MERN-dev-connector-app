e6b37527f0c612ca22f2ade233704cf46d0e4c92
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 00:51:33 -0500

    Delete Profile Education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -288,5 +288,25 @@ router.put(
   }
 );
 
+//@route    DELETE api/profile/education/"edu_id
+//@desc     Delete Profile Education 
+//@access   Private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get the remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
+
 
 module.exports = router;
