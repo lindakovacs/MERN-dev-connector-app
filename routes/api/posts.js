24a85c931ae4dca77a485f8da1eccfdc334cde35
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 15:02:12 -0500

    Unlike a Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -130,4 +130,30 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/posts/unlike/:id
+// @desc     Unlike a Post
+// @access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has not been liked yet
+    if (!post.likes.some(like => like.user.toString() === req.user.id)) {
+      return res.status(400).json({ msg: 'Post has not been liked yet' });
+    }
+
+    // Remove the like
+    post.likes = post.likes.filter(
+      ({ user }) => user.toString() !== req.user.id
+    );
+
+    await post.save();
+
+    return res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
