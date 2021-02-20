5db47d860975bf645be65a986bebd7ad9bee7801
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 18:41:45 -0500

    handle State Register Component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,46 +1,89 @@
-import React, { Fragment } from 'react';
+ import React, { Fragment, useState } from 'react';
 
 const Register = () => {
-    return (
-      <Fragment>
-        <h1 className='large text-primary'>Sign Up</h1>
-        <p className='lead'>
-          <i className='fas fa-user'></i> Create Your Account
-        </p>
-        <form className='form' action='create-profile.html'>
-          <div className='form-group'>
-            <input type='text' placeholder='Name' name='name' required />
-          </div>
-          <div className='form-group'>
-            <input type='email' placeholder='Email Address' name='email' />
-            <small className='form-text'>
-              This site uses Gravatar so if you want a profile image, use a
-              Gravatar email
-            </small>
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              placeholder='Password'
-              name='password'
-              minLength='6'
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              placeholder='Confirm Password'
-              name='password2'
-              minLength='6'
-            />
-          </div>
-          <input type='submit' className='btn btn-primary' value='Register' />
-        </form>
-        <p className='my-1'>
-          Already have an account? <a href='login.html'>Sign In</a>
-        </p>
-      </Fragment>
-    );
+  // The way of doing it before
+  //state = {
+  //   formData : {
+  //   name: '',
+  //   email: '',
+  //   password: '',
+  //   password2:''
+  //   }
+  // }
+  // this.setState = state;
+  // Same as below
+
+  const [formData, setFormdata] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  // Pull name, email, passowrd from formData
+  const { name, email, password, password2 } = formData;
+
+  const onchange = (e) =>
+    setFormdata({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' action='create-profile.html'>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={(e) => onchange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onchange(e)}
+            required
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onchange(e)}
+            minLength='6'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            value={password2}
+            onChange={(e) => onchange(e)}
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Register;
