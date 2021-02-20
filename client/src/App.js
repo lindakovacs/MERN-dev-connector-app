091ae7a6e204796efbecc6956fa25f4bdf467160
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 17:33:42 -0500

    Implement Login and Register Components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
@@ -10,7 +12,13 @@ function App() {
     <Router>
       <Fragment>
         <Navbar />
-        <Landing />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );
