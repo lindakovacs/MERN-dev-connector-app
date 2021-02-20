1d8b719781338a0110485a590059060789a346bd
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 15:45:39 -0500

    Delete Post Comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -189,4 +189,36 @@ router.post(
     }
 });
 
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete Post Comment
+// @access   Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    // Check if comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+    // Check if user is owner
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
+    await post.save();
+    return res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
