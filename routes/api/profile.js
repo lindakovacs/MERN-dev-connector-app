1d677239c7953edec7aa9829d575427fa3d0b505
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-20 00:48:39 -0500

    Add Profile Education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -237,4 +237,56 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 });
 
+//@route    PUT api/profile/education
+//@desc     Add Profile Education
+//@access   Private
+
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'Title is required').not().isEmpty(),
+      check('degree', 'Degree is required').not().isEmpty(),
+      check('fieldofstudy', 'Field of Study is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
+
+
 module.exports = router;
