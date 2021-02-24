60953f6fe5a9c9e01f48a749d62b0b49af9b55df
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-23 21:10:08 -0500

    Remove Alert Reducer

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 3000) => (dispatch) => {
   const id = uuid();
   dispatch({
     type: SET_ALERT,
@@ -11,4 +11,14 @@ export const setAlert = (msg, alertType) => (dispatch) => {
       id,
     },
   });
+  
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id,
+      }),
+    timeout
+  );
+
 };
