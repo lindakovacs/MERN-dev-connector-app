3035d51adf52291d61382befe264be087748a97b
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-24 23:28:11 -0500

    Add Login Action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
   REGISTER_SUCCESS,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -18,11 +20,11 @@ export const loadUser = () => async (dispatch) => {
     
     dispatch({
       type: USER_LOADED,
-      payload: res.data
+      payload: res.data,
     }); 
   } catch (err) {
     dispatch({
-      type: AUTH_ERROR
+      type: AUTH_ERROR,
     }); 
   }
 }
@@ -44,6 +46,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -55,3 +58,34 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     });
   }
 };
+
+//Login User
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+
+    } catch (err) {
+      const errors = err.response.data.errors;
+      if(errors) {
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      }
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+    }
+}
