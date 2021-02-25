5b9bd11d723e981cd86728898d9a09a1cdbcbc05
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-25 18:27:45 -0500

    Implement Private Route and Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
 //Redux
@@ -33,6 +35,7 @@ function App() {
             <Switch>
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = () => {
+    return <div>Dashboard</div>;
+}
+
+Dashboard.propTypes = {};
+export default Dashboard
