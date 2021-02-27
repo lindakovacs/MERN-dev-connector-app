bd5f2d047ac09a4141350beaaf533f9d6ee8f2ee
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-27 11:28:52 -0500

    Integrate Components

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
-
+import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
@@ -40,6 +40,7 @@ function App() {
             <Switch>
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
+              <Route exact path='/profiles' component={Profiles} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
