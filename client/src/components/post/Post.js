ba5f74daa889d8de1bc688f45b478a1bb8565486
Linda Kovacs <lindaprovenza@yahoo.com>
2021-02-27 14:17:32 -0500

    Implement Post Component

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,35 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPosts } from '../../actions/post';
+
+const Post = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'>Welcome to the community</i>
+      </p>
+      <div className='posts'>
+          <p>Posts</p>
+      </div>
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPosts })(Post);
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -32,7 +32,7 @@ const Profile = ({
           {auth.isAuthenticated &&
             auth.loading === false &&
             auth.user._id === profile.user._id && (
-              <Link to='edit-profile' className='btn btn-dark'>
+              <Link to='/edit-profile' className='btn btn-dark'>
                 Edit Profile
               </Link>
             )}
